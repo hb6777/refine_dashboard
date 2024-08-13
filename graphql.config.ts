386d6ac577d57c1122549ea6a3d2d5545de5e1b7
Renee L <Reneeliew2002@yahoo.com>
2024-08-13 15:21:40 -0400

    retest

--- graphql.config.ts
+++ graphql.config.ts
@@ -24,8 +24,7 @@ const config: IGraphQLConfig = {
         },
         "src/graphql/types.ts": {
           preset: "import-types",
-          documents: ["./*.{ts,tsx}"],
-         // documents: ["src/**/*.{ts,tsx}"],
+           documents: ["src/**/*.{ts,tsx}"],
           plugins: ["typescript-operations"],
           config: {
             skipTypename: true,
